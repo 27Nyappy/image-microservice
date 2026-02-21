45699a5f717a91a5cd32245d3e4383b7161dbadb
27Nyappy <antonella.gr27@gmail.com>
2026-02-20 20:49:28 -0800

    stacked frames into single row frames

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,14 +1,27 @@
 import io
 from PIL import Image
 
-def image_processing(asset_path, is_cropped, frames, w, h, start_y, start_x = 0):
+def image_processing(asset_path, is_cropped, frames, w, h, start_y, start_x = 0, is_stacked = False):
 	with Image.open(asset_path) as im:
 		strip = im
 
 		if not is_cropped:
+			even_frames = frames % 2 == 0
+			top_frames = 2 if is_stacked else frames
+			y_2 = start_y + (1 * h)
 			# crop based on image box coordinates, as per Pillow docs:
 			# Rectangles are represented as 4-tuples, (x1, y1, x2, y2), with the upper left corner given first.
-			strip = im.crop((start_x, start_y, start_x + (frames * w), start_y + (1 * h)))
+			strip = im.crop((start_x, start_y, start_x + (top_frames * w), y_2))
+
+			if is_stacked:
+				bottom_frames = frames - top_frames
+				# if frames are stacked extract the bottom frames
+				bottom_strip = im.crop((start_x, y_2, start_x + (bottom_frames * w), y_2 + h))
+				merged_strip = Image.new(im.mode, ((top_frames + bottom_frames) * w, h))
+				# merge the two stacked frames into a single row of frames
+				merged_strip.paste(strip)
+				merged_strip.paste(bottom_strip, (top_frames * w, 0))
+				strip = merged_strip
 
 		buffer = io.BytesIO() # setup buffer to hold WebP image
 		strip.save(buffer, format="WEBP", lossless=True, quality=100, method=6)
